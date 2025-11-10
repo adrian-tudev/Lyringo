80c2b9ac6b1a2192bce851f731aaee97db7c5c29
Daniel Zema <fenton@chalmers.se>
2025-11-10 12:26:15 +0100

    Implemented get_song_lyrics

--- genius_client.py
+++ genius_client.py
@@ -6,3 +6,11 @@ load_dotenv()
 token = os.getenv("GENIUS_ACCESS_TOKEN")
 genius = lyricsgenius.Genius(token)
 
+# Look up lyrics for a given artist and song
+def get_song_lyrics(song_title, artist):
+    song = genius.search_song(song_title, artist)
+    if song:
+        lyrics = genius.lyrics(song)
+        return lyrics
+    else: 
+        return (f"Lyrics for {song_title} not found")
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import spotify_client
 import genius_client
 
 def main():
-    print(spotify_client.get_token)
+    print(genius_client.get_song_lyrics("PUNTO G", "Quevedo"))
 
 if __name__ == "__main__":
     main()
